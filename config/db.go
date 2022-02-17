03b5610b6e201be37b1e4def9411ae92f9e4af23
Bryan Wahyu <bryanwahyukp95@gmail.com>
2022-02-17 09:55:40 +0700

    fixbug

--- config/db.go
+++ config/db.go
@@ -8,10 +8,9 @@ func SetupDB() *gorm.DB{
 	user:="root"
 	pass:=""
 	port:="3306"
-	host:="locahost"
+	host:="localhost"
 	dbname:="blog"
-	
-	URL := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", user, pass,host ,port,dbname)
+	URL := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local",user,pass,host,port,dbname)
 	db, err := gorm.Open("mysql", URL)
 	if err != nil {
 		panic(err.Error())
